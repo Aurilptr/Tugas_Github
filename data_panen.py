efb3add9e20fd8794c8ec9fc03c1cc0c38f1c79d
Aurilptr <auril.putri@mhs.itenas.ac.id>
2024-11-30 14:34:18 +0700

    Update data_panen.py

--- data_panen.py
+++ data_panen.py
@@ -49,8 +49,7 @@ for key, value in data_panen.items():
 # 2. Menampilkan jumlah hasil panen jagung dari lokasi2
 print(f"\nHasil Panen Jagung dari lokasi2: {data_panen['lokasi2']['hasil_panen']['jagung']}")
 
-# 3. Menampilkan nama lokasi dari lokasi3
-print(f"\nNama Lokasi dari lokasi3: {data_panen['lokasi3']['nama_lokasi']}")
+print("15-2023-023")
 
 # 4. Masukkan jumlah hasil panen padi dan kedelai ke dalam variabel yang berbeda
 # 5. buat variabel terpisah untuk menyimpan jumlah hasil panen padi dan kedelai dari setiap lokasi
